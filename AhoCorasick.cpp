1d509ce2086b51267a1610ff65de604f84af3a04
renamoe <true-banana@outlook.com>
2021-10-24 00:00:26 +0800

    add ac, fenwick

---
+++ AhoCorasick.cpp
@@ -0,0 +1,48 @@
+struct AC_Automaton {
+    static constexpr int SIG = 26;
+    struct Node {
+        Node() : ch{}, fail{} {}
+        Node *ch[SIG];
+        Node *fail;
+        // some infomation
+    };
+    AC_Automaton(int size) : pool(size + 1), cnt_pool(1), root(pool.data()) {}
+    Node *new_node() {
+        return pool.data() + (cnt_pool++);
+    }
+    Node *insert(const std::string &s) {
+        Node *p = root;
+        for (char _c : s) {
+            int c = _c - 'a';
+            if (!p->ch[c]) p->ch[c] = new_node();
+            p = p->ch[c];
+        }
+        return p;
+    }
+    void build_fail() {
+        std::queue<Node *> q;
+        for (int i = 0; i < SIG; ++i) {
+            if (root->ch[i]) {
+                root->ch[i]->fail = root;
+                q.push(root->ch[i]);
+            } else {
+                root->ch[i] = root;
+            }
+        }
+        while (!q.empty()) {
+            Node *u = q.front();
+            q.pop();
+            for (int i = 0; i < SIG; ++i) {
+                if (u->ch[i]) {
+                    u->ch[i]->fail = u->fail->ch[i];
+                    q.push(u->ch[i]);
+                } else {
+                    u->ch[i] = u->fail->ch[i];
+                }
+            }
+        }
+    }
+    std::vector<Node> pool;
+    int cnt_pool;
+    Node *root;
+};
---
+++ Fenwick.cpp
@@ -0,0 +1,33 @@
+// 单点加，前缀求和
+
+struct Fenwick {
+    int n;
+    std::vector<int> s;
+    Fenwick(int _n) : n(_n), s(_n) {}
+    void add(int x, int k) {
+        for (int i = x + 1; i <= n; i += i & -i)
+            s[i - 1] += k;
+    }
+    int query(int x) {
+        int res = 0;
+        for (int i = x + 1; i; i -= i & -i)
+            res += s[i - 1];
+        return res;
+    }
+};
+
+template<typename T> struct Fenwick {
+    int n;
+    std::vector<T> s;
+    Fenwick(int _n) : n(_n), s(_n) {}
+    void add(int x, T k) {
+        for (int i = x + 1; i <= n; i += i & -i)
+            s[i - 1] += k;
+    }
+    T query(int x) {
+        T res = 0;
+        for (int i = x + 1; i; i -= i & -i)
+            res += s[i - 1];
+        return res;
+    }
+};

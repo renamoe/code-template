4ab3e7479e52ef5591abced1ea4b426bbcaa61f4
RenaMoe <72136214+renamoe@users.noreply.github.com>
2022-03-17 17:15:02 +0800

    Update Fenwick.cpp

--- Fenwick.cpp
+++ Fenwick.cpp
@@ -1,5 +1,47 @@
 #include <vector>
 
+template<class T> struct Fenwick {
+    int n;
+    std::vector<T> s;
+
+    Fenwick(int n) : n(n), s(n) {}
+    void modify(int x, const T &k) {
+        while (x < n) {
+            s[x] += k;
+            x += ~x & (x + 1);
+        }
+    }
+    T query(int x) {
+        T res = 0;
+        while (~x) {
+            res += s[x];
+            x -= ~x & (x + 1);
+        }
+        return res;
+    }
+};
+
+template<class T> struct FenwickCdq {
+    int n;
+    std::vector<std::vector<T>> s;
+
+    FenwickCdq(int n) : n(n), s(n) {}
+    void modify(int x, const T &k) {
+        while (x < n) {
+            s[x].push_back(k);
+            x += ~x & (x + 1);
+        }
+    }
+    void query(int x, const T &k) {
+        while (~x) {
+            s[x].push_back(k);
+            x -= ~x & (x + 1);
+        }
+    }
+};
+
+// Old
+
 struct Fenwick {
     int n;
     std::vector<int> s;

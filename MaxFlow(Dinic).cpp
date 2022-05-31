954eca8f6b5652b4db6a069d985fe47cabff6671
RenaMoe <72136214+renamoe@users.noreply.github.com>
2022-05-31 15:00:20 +0800

    Update MaxFlow(Dinic).cpp

--- MaxFlow(Dinic).cpp
+++ MaxFlow(Dinic).cpp
@@ -1,3 +1,72 @@
+template<class T = int> struct Dinic {
+    static constexpr T Inf = std::numeric_limits<T>::max();
+    struct Edge {
+        int to;
+        T cap;
+    };
+    int n;
+    std::vector<std::vector<int>> g;
+    std::vector<Edge> e;
+    std::vector<int> cur;
+    std::vector<int> h;
+
+    Dinic(int n) : n(n), g(n) {}
+    void addFlow(int u, int v, T w) {
+        g[u].push_back(e.size());
+        e.push_back({v, w});
+        g[v].push_back(e.size());
+        e.push_back({u, 0});
+    }
+    bool bfs(int s, int t) {
+        h.assign(n, -1);
+        std::queue<int> q;
+        h[s] = 0;
+        q.push(s);
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (int i : g[u]) {
+                int v = e[i].to;
+                if (e[i].cap > 0 && h[v] == -1) {
+                    h[v] = h[u] + 1;
+                    if (v == t) return true;
+                    q.push(v);
+                }
+            }
+        }
+        return false;
+    }
+    T dfs(int u, int t, T f) {
+        if (u == t || !f) {
+            return f;
+        }
+        T r = f;
+        for (int &i = cur[u]; i < int(g[u].size()); ++i) {
+            int j = g[u][i];
+            int v = e[j].to;
+            if (e[j].cap > 0 && h[v] == h[u] + 1) {
+                T l = dfs(v, t, std::min(r, e[j].cap));
+                e[j].cap -= l;
+                e[j ^ 1].cap += l;
+                r -= l;
+                if (!r) break;
+            }
+        }
+        return f - r;
+    }
+    T maxFlow(int s, int t) {
+        T res = 0;
+        while (bfs(s, t)) {
+            cur.assign(n, 0);
+            res += dfs(s, t, Inf);
+        }
+        return res;
+    }
+};
+
+
+// old
+
 template<typename FT> struct Graph {
     static constexpr FT Inf = std::numeric_limits<FT>::max() / 3;
     std::vector<int> head;

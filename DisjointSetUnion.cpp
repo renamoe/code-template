290209f7b73cb3e9fbd6b4385246b94bda71fad5
RenaMoe <72136214+renamoe@users.noreply.github.com>
2022-06-19 09:03:45 +0800

    Create DisjointSetUnion.cpp

--- DisjointSetUnion.cpp
+++ DisjointSetUnion.cpp
@@ -10,3 +10,26 @@ struct DisjointSetUnion {
         return fa[x] == x ? x : (fa[x] = find(fa[x]));
     }
 };
+
+// 数组版
+
+template<int N> struct DSU {
+    int fa[N];
+
+    void init(int n) {
+        std::fill_n(fa, n, -1);
+    }
+    int find(int x) {
+        if (fa[x] == -1) return x;
+        return fa[x] = find(fa[x]);
+    }
+    bool join(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            fa[x] = y;
+            return true;
+        }
+        return false;
+    }
+};

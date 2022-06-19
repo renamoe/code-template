398bd69a96e51180cea43a622c17fb0add148d9b
RenaMoe <72136214+renamoe@users.noreply.github.com>
2022-06-19 09:05:31 +0800

    Update DisjointSetUnion.cpp

--- DisjointSetUnion.cpp
+++ DisjointSetUnion.cpp
@@ -1,13 +1,23 @@
 #include <vector>
-#include <numeric>
 
-struct DisjointSetUnion {
+// vector 版
+
+struct DSU {
     std::vector<int> fa;
-    DisjointSetUnion(int n) : fa(n) {
-        std::iota(fa.begin(), fa.end(), 0);
-    }
+
+    DSU(int n) : fa(n, -1) {}
     int find(int x) {
-        return fa[x] == x ? x : (fa[x] = find(fa[x]));
+        if (fa[x] == -1) return x;
+        return fa[x] = find(fa[x]);
+    }
+    bool join(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            fa[x] = y;
+            return true;
+        }
+        return false;
     }
 };
 
